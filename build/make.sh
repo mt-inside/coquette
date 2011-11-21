22458158aeeabf155623e43fb126335bcbe666cc
matt <matt>
2011-11-21 13:53:46 +0100

    Impliment COM interface for UNIX systems. Use stdio interface by default for testing.

--- build/make.sh
+++ build/make.sh
@@ -1,1 +1,1 @@
-gcc -o consult -pg -g3 -O0 -Wall -Wextra -pedantic ../src/*.c ../src/com/*.c
+gcc -o consult -pg -g3 -O0 -Wall -Wextra -pedantic ../src/*.c ../src/com/com_stdio.c
--- src/com/com_unix.c
+++ src/com/com_unix.c
@@ -1,47 +1,42 @@
-#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "com.h"
 
 
+static int s_tty;
+
+
+int com_init( char *tty_dev_name )
+{
+    s_tty = open( tty_dev_name, O_RDWR | O_EXLOCK );
+
+    return( s_tty != -1 );
+}
+
+int com_finalise( void )
+{
+    return close( s_tty );
+}
+
+
 int com_send_byte( uint8_t byte )
 {
-    printf( "COM_STDIO > 0x%02x\n", byte );
-    return 1;
+    return( com_send_bytes( &byte, 1 ) );
 }
 
 int com_send_bytes( uint8_t *bytes, unsigned count )
 {
-    unsigned i;
-
-    for( i = 0; i < count; ++i )
-    {
-        com_send_byte( bytes[i] );
-    }
-
-    return count;
+    return( write( s_tty, bytes, count ) == count );
 }
+
 
 int com_read_byte( uint8_t *byte )
 {
-    unsigned x;
-
-    printf( "COM STDIO < 0x" );
-
-    scanf( "%x", &x );
-    *byte = (uint8_t)x;
-
-    return 1;
+    return( com_read_bytes( byte, 1 ) );
 }
-
 
 int com_read_bytes( uint8_t *bytes, unsigned count )
 {
-    unsigned i;
-
-    for( i = 0; i < count; ++i )
-    {
-        com_read_byte( &(bytes[i]) );
-    }
-
-    return count;
+    return( read( s_tty, bytes, count ) == count );
 }
