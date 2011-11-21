333118a1c30d18c3695fd5bf85e318104fdfe602
matt <matt>
2011-11-21 13:54:40 +0100

    Extend test frontend to call other functions.

--- src/fe_test.c
+++ src/fe_test.c
@@ -4,12 +4,9 @@
 #include "commands.h"
 
 
-int main( int argc, char **argv )
+static int read_faults( void )
 {
     fault_report_t *faults, *fault;
-
-    (void)argc;
-    (void)argv;
 
     read_dtc( &faults );
     fault = faults;
@@ -22,6 +19,55 @@ int main( int argc, char **argv )
     }
 
     faults_free( faults );
+
+    return 0;
+}
+
+static int read_regs( void )
+{
+    int reg;
+
+    /* read_register( reg_TACHO, &reg ); */
+    read_register( reg_engine_COOLANT_TEMP, &reg );
+
+    printf( "%d $unit\n", reg );
+
+    return 0;
+}
+
+static int read_mem( void )
+{
+    uint8_t data;
+
+    peek_memory( 0x1234, &data );
+
+    printf( "byte at 0x1234: 0x%02x\n", data );
+
+    return 0;
+}
+
+static int read_bit( void )
+{
+    int bit;
+
+    read_flag( bit_FUEL_PUMP_RELAY, &bit );
+
+    printf( "flag: %s\n", bit ? "on" : "off" );
+
+    return 0;
+}
+
+int main( int argc, char **argv )
+{
+    (void)argc;
+    (void)argv;
+
+    com_init( NULL );
+    handshake( ecu_ENGINE );
+
+    read_bit( );
+
+    com_finalise( );
 
     return 0;
 }
