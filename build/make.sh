faf4eab09415dd69c3c2a95331d0675b7eadc735
matt <matt>
2011-12-22 15:01:56 +0000

    We now need to link against libmath (libm). fe_avg may as well be the default fromend as it's the future.

--- build/make.sh
+++ build/make.sh
@@ -1,1 +1,1 @@
-gcc -o consult -pg -g3 -O0 -Wall -Wextra -pedantic -I../src ../src/*.c ../src/fe/fe_simple.c ../src/com/com_stdio.c
+gcc -o consult -pg -g3 -O0 -Wall -Wextra -pedantic -I../src -lm ../src/*.c ../src/fe/fe_avg.c ../src/com/com_stdio.c
