484afaa7cbf13ba0132400ce90093b9ad36792bc
matt <matturner@gmail.com>
2012-05-20 12:51:23 +0100

    Give windows a timersub definition
    And remove the seconds-resolution hack.

--- src/common.h
+++ src/common.h
@@ -3,6 +3,30 @@
 #define LOG( x ) printf( x "\n" )
 
 #ifdef WIN32
+
 #include "Windows.h"
+
 #define sleep(x) Sleep(1000 * (x))
+
+# define timeradd(a, b, result)						      \
+  do {									      \
+    (result)->tv_sec = (a)->tv_sec + (b)->tv_sec;			      \
+    (result)->tv_usec = (a)->tv_usec + (b)->tv_usec;			      \
+    if ((result)->tv_usec >= 1000000)					      \
+      {									      \
+	++(result)->tv_sec;						      \
+	(result)->tv_usec -= 1000000;					      \
+      }									      \
+  } while (0)
+
+# define timersub(a, b, result)						      \
+  do {									      \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;			      \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;			      \
+    if ((result)->tv_usec < 0) {					      \
+      --(result)->tv_sec;						      \
+      (result)->tv_usec += 1000000;					      \
+    }									      \
+  } while (0)
+
 #endif
