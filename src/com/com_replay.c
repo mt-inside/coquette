ebb0060104ab5dbd8ec695527e62238d278e87a9
matt <matturner@gmail.com>
2012-05-20 12:01:47 +0100

    COM replay to simulate shutting down.

--- src/com/com_replay.c
+++ src/com/com_replay.c
@@ -2,26 +2,45 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include "com.h"
+#include "commands.h"
+#include "consult_constants.h"
 
+
+typedef enum
+{
+    state_RUNNING,
+    state_STOPPING,
+    state_FINALISED
+} state_t;
 
 static FILE *s_file;
+static state_t s_state;
 
 
 int com_init( char *path )
 {
+    s_state = state_RUNNING;
     return( !( s_file = fopen( path, "r" ) ) );
 }
 
 int com_finalise( void )
 {
+    s_state = state_FINALISED;
     return fclose( s_file );
 }
 
 
 int com_send_byte( uint8_t byte )
 {
+    if( byte == cmd_STOP )
+    {
+        assert( s_state == state_RUNNING );
+        s_state = state_STOPPING;
+    }
+
     printf( "COM_REPLAY > 0x%02x\n", byte );
 
     return 0;
@@ -46,8 +65,18 @@ int com_read_byte( uint8_t *byte )
 
     usleep(100 * 1000);
 
-    fscanf( s_file, "%x ", &x );
-    *byte = (uint8_t)x;
+    switch( s_state )
+    {
+        case state_STOPPING:
+            *byte = c_end_of_response;
+            break;
+        case state_RUNNING:
+            fscanf( s_file, "%x ", &x );
+            *byte = (uint8_t)x;
+            break;
+        default:
+            assert( 0 );
+    }
 
     printf( "COM_REPLAY < 0x%02x\n", *byte );
 
