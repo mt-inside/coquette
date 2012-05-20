3914113fce4531328025c0c8c1a8d9daf634854b
matt <matturner@gmail.com>
2012-05-21 00:58:17 +0100

    Observer_shift now has configurable number of warning levels.

--- src/observers/observer_shift.c
+++ src/observers/observer_shift.c
@@ -13,6 +13,7 @@ struct _observer_shift_t
     observer_base_t base;
     unsigned warn_start;
     unsigned warn_end;
+    unsigned intervals;
     unsigned warn_level;
     unsigned warn_level_old;
 };
@@ -22,7 +23,8 @@ static void observer_shift_update( observer_base_t *this, int first_time );
 observer_shift_t *observer_shift_new(
     engine_reg_t reg,
     observer_cb_t cb, void *ctxt,
-    unsigned warn_start, unsigned warn_end
+    unsigned warn_start, unsigned warn_end,
+    unsigned intervals
 )
 {
     observer_shift_t *this = calloc( sizeof( observer_shift_t ), 1 );
@@ -31,6 +33,7 @@ observer_shift_t *observer_shift_new(
 
     this->warn_start = warn_start;
     this->warn_end   = warn_end;
+    this->intervals  = intervals;
 
     return this;
 }
@@ -43,9 +46,12 @@ static void observer_shift_update( observer_base_t *obs, int first_time )
     assert( obs->class == observer_subclass_SHIFT );
 
     /* mmm, branch free... */
-    w = (int)ceil((double)((signed)obs->value - (signed)this->warn_start) / (((signed)this->warn_end - (signed)this->warn_start) / 3));
+    w = (int)ceil(
+            (double)((signed)obs->value - (signed)this->warn_start) /
+            (((signed)this->warn_end - (signed)this->warn_start) / this->intervals)
+        );
     w = MAX( w, 0 );
-    w = MIN( w, 4 );
+    w = MIN( w, (signed)this->intervals + 1 );
 
     this->warn_level = w;
 
--- src/observers/observer_shift.h
+++ src/observers/observer_shift.h
@@ -9,13 +9,16 @@ typedef struct _observer_shift_t observer_shift_t;
 extern observer_shift_t *observer_shift_new(
     engine_reg_t reg,
     observer_cb_t, void *ctxt,
-    unsigned warn_start, unsigned warn_end
+    unsigned warn_start, unsigned warn_end,
+    /* number of steps "in the middle". E.g. for 3 intervals there are 5 states:
+     * under, interval[1,2,3], over */
+    unsigned intervals
 );
 
-/* gets the shift warning level, [0-4]
- *              x < warn_start  => 0   - no warning
- * warn_start < x < warn_end    => 1-3 - warning
- * warn_end   < x               => 4   - shift right now
+/* gets the shift warning level, [0 - intervals+1]
+ *              x < warn_start  => 0           - no warning
+ * warn_start < x < warn_end    => 1-intervals - warning
+ * warn_end   < x               => intervals+1 - shift right now
  */
 extern void observer_shift_get_shift( observer_base_t *obs, unsigned *warn_level );
 
