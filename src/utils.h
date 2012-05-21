56ace1f45ebfa72fb93964e567a999492fb36844
matt <matt>
2012-05-21 18:37:26 +0100

    Add sys/time.h to header using struct timeval.
    Morally is should be there, and indeed stream_frame.c won't build
    without it on Windows. Was OK on Linux, presumably pthread.h or
    something includes it on that platform.

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,8 @@
 #ifndef _INCLUDED_UTILS_H
 #define _INCLUDED_UTILS_H
 
+#include "sys/time.h"
+
 typedef void (*mapper_fn_t)( void *in, void *out );
 extern void *map( void *in, unsigned in_stride, unsigned out_stride, unsigned count, mapper_fn_t mapper );
 
